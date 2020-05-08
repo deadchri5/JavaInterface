4b5ffe2f07b42e43506132fa92e0803341b3f871
deadchri5 <deadchri5h@gmail.com>
2020-05-07 22:17:40 -0500

    Prueba de base

---
+++ Documents/NetBeansProjects/JavaInterface/src/javainterface/Consultas.java
@@ -0,0 +1,10 @@
+package javainterface;
+import java.sql.*;
+
+public class Consultas{
+    
+    public static void SqlConsulta() throws ClassNotFoundException, SQLException{
+           
+    }
+    
+}
