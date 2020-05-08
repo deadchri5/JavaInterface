c21dcc168c600548d609772ca72539a15fb7621f
deadchri5 <deadchri5h@gmail.com>
2020-05-07 22:23:25 -0500

    prueba de base 2.0 pana

--- Documents/NetBeansProjects/JavaInterface/src/javainterface/Consultas.java
+++ Documents/NetBeansProjects/JavaInterface/src/javainterface/Consultas.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class Consultas{
     
     public static void SqlConsulta() throws ClassNotFoundException, SQLException{
-           
+           //Aqui van las consultas
     }
     
 }
