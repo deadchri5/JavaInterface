02329dfc4ee9bdd6cdc618db91d9e36ff7e2153e
Mariomvm <mario00375@gmail.com>
2020-05-10 01:23:07 -0500

    Conexion con base

--- Documents/NetBeansProjects/JavaInterface/src/javainterface/Consultas.java
+++ Documents/NetBeansProjects/JavaInterface/src/javainterface/Consultas.java
@@ -1,12 +1,40 @@
 package javainterface;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.Statement;
 
 public class Consultas{
     
-    public static void SqlConsulta() throws ClassNotFoundException, SQLException{
-           //Aqui van las consultas
-           //asujdfhqpewubfa
-           //alijsgbpa
+    static Connection contacto = null;
+    
+    public static  Connection getConexion(){
+        String url = "jdbc:sqlserver://DESKTOP-0J6KP8T\\NATIONALSOFT:1433;databaseName=softrestaurant95pro";
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        }catch (ClassNotFoundException e){
+            JOptionPane.showMessageDialog(null , "no se establecion conexion" +
+                    e.getMessage(),"error de conexion", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        try{
+            contacto = DriverManager.getConnection(url, "sa", "National09"); 
+        }
+        catch (SQLException e){
+            JOptionPane.showMessageDialog(null , "Error" +
+                    e.getMessage(),"error de conexion", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        return contacto;
+        
     }
+    
+    
+    public static void SqlConsulta() throws ClassNotFoundException, SQLException{
+           
+    }
+
     
 }
