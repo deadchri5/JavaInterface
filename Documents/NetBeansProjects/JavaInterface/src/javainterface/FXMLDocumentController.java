49dfe6c3bea96dc7f372e1bf5d6335f10788d161
deadchri5 <deadchri5h@gmail.com>
2020-05-16 23:48:43 -0500

    Busqueda de folios por Fecha Hecho

--- Documents/NetBeansProjects/JavaInterface/src/javainterface/FXMLDocumentController.java
+++ Documents/NetBeansProjects/JavaInterface/src/javainterface/FXMLDocumentController.java
@@ -1,6 +1,5 @@
 package javainterface;
 
-import java.util.ArrayList;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -12,46 +11,59 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javax.swing.JOptionPane;
 
 public class FXMLDocumentController implements Initializable {
-    
+
     ObservableList list = FXCollections.observableArrayList();
-    
+
     @FXML
     private Label label;
     @FXML
-    private ListView <String> listaCheques; 
-    
+    private DatePicker fechaInicio, fechaFin;
+    @FXML
+    private ListView<Integer> listaCheques;
+
     @FXML
     private void aceptar(ActionEvent event) {
         System.out.println("You clicked me!");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //TODO
-        obtenerCheques();
-    }    
-    
-    public void obtenerCheques(){
-        list.removeAll(list);
         listaCheques.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        ResultSet folios;
-        try{
-        folios = Consultas.Consulta("SELECT folio FROM cheques");
-        while (folios.next()){
-            int folio = folios.getInt(1);
-            list.add("Folio #" + folio);
-            listaCheques.getItems().addAll(list);
-        }
-        }
-        catch(SQLException e){
-            JOptionPane.showMessageDialog(null , "Ocurrio un error al obtener folios" +
-                    e.getMessage(),"Error de consulta", JOptionPane.ERROR_MESSAGE);
+
+    }
+
+    public void obtenerCheques() {
+        list.removeAll(list);
+        if (fechaInicio.getValue() == null || fechaFin.getValue() == null) {
+            JOptionPane.showMessageDialog(null, "Ambos campos de fecha deben de estar llenos");
+        } else {
+            String query = "SELECT folio FROM cheques where fecha >= '"
+                    + fechaInicio.getValue() + " 00:00:00' AND cierre <= '" + fechaFin.getValue() + " 23:59:00'";
+            ResultSet folios;
+            try {
+                folios = Consultas.Consulta(query);
+                while (folios.next()) {
+                    int folio = folios.getInt(1);
+                    System.out.println(folio);
+                    list.add(folio);
+                    listaCheques.getItems().addAll(list);
+                }
+                if (list.size() == 0){
+                    JOptionPane.showMessageDialog(null, "No se encontraron folios entre las fechas " + fechaInicio.getValue() + 
+                            " y " + fechaFin.getValue());
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Ocurrio un error al obtener folios"
+                        + e.getMessage(), "Error de consulta", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
-    
+
 }
